f3da09e56c1a319be69a96478aadb22b5979fd16
Giridharan <208488126+gir1x@users.noreply.github.com>
2026-09-11 03:05:17 +0530

    Enable asynchronous processing in WmsApplication

--- src/main/java/com/example/wms/WmsApplication.java
+++ src/main/java/com/example/wms/WmsApplication.java
@@ -2,8 +2,10 @@ package com.example.wms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class WmsApplication {
     public static void main(String[] args) {
         SpringApplication.run(WmsApplication.class, args);
